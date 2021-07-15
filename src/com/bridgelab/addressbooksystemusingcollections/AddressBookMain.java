0fb640aa1b3032f22a042c431eeaa7fda8283afc
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-15 23:48:28 +0530

    [mihir] Added: new contact from commandline and storing details in address book.

--- src/com/bridgelab/addressbooksystemusingcollections/AddressBookMain.java
+++ src/com/bridgelab/addressbooksystemusingcollections/AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.bridgelab.addressbooksystemusingcollections;
 
+import java.util.Scanner;
+
 /********************************************************
  * @author mihir
  *
@@ -10,8 +12,52 @@ package com.bridgelab.addressbooksystemusingcollections;
 
 public class AddressBookMain
 {
+    Scanner scanner;
+
+    public AddressBookMain()
+    {
+        scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Name : chooseOption
+     *
+     * Description : Displaying option to user to choose from.
+     *
+     * Algorithm : In this method displaying user multiple option from switch statement.
+     * according to users choice respective method will get called.
+     *
+     * Modification : First commit 14-July-2021
+     */
+    public void chooseOption()
+    {
+        AddressBookService addressBookService = new AddressBookService();
+        boolean isExit = false;
+        while (!isExit)
+        {
+            System.out.print("Enter Number As Per Your Choice:\n1.Add-New Contacts:\n2.Display-All Records:\n3.Exit:\n");
+            int userInput = scanner.nextInt();
+            switch (userInput)
+            {
+                case 1:
+                        addressBookService.addNewContact();
+                        break;
+                case 2:
+                        addressBookService.displayContacts();
+                        break;
+                case 3:
+                        //Exit from program.
+                        isExit = true;
+                        break;
+                default:
+                        System.out.println("Please enter valid input");
+            }
+        }
+    }
+
     public static void main(String[] args)
     {
-
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.chooseOption();
     }
 }
--- src/com/bridgelab/addressbooksystemusingcollections/AddressBookService.java
+++ src/com/bridgelab/addressbooksystemusingcollections/AddressBookService.java
@@ -1,5 +1,8 @@
 package com.bridgelab.addressbooksystemusingcollections;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /************************************************************************
  * @author mihir
  *
@@ -11,5 +14,77 @@ package com.bridgelab.addressbooksystemusingcollections;
 
 public class AddressBookService
 {
+    Scanner scanner;
+    //To store contacts.
+    ArrayList<PersonDetails> contactList;
 
+    //instantiating scanner and ArrayList in constructor.
+    public AddressBookService()
+    {
+        scanner = new Scanner(System.in);
+        contactList = new ArrayList<>();
+    }
+
+    /**
+     * Name : addNewContact
+     *
+     * Description : Adding new contact to AddressBook.
+     *
+     * Algorithm : In this method accepting all necessary contact details from user,
+     * and adding those all details to store in AddressBook as a new contact.
+     *
+     * Modification : First commit 14-July-2021.
+     */
+    public void addNewContact()
+    {
+            PersonDetails contactDetails = new PersonDetails();
+
+            System.out.print("Enter First Name : ");
+            contactDetails.setFirstName(scanner.next());
+
+            System.out.print("Enter Last Name : ");
+            contactDetails.setLastName(scanner.next());
+
+            System.out.print("Enter Address : ");
+            contactDetails.setAddress(scanner.next());
+
+            System.out.print("Enter City : ");
+            contactDetails.setCity(scanner.next());
+
+            System.out.print("Enter State : ");
+            contactDetails.setState(scanner.next());
+
+            System.out.print("Enter ZipCode : ");
+            contactDetails.setZipCode(scanner.nextInt());
+
+            System.out.print("Enter Phone-Number : ");
+            contactDetails.setPhoneNumber(scanner.nextLong());
+
+            System.out.print("Enter Email-Id : ");
+            contactDetails.setEmailId(scanner.next());
+
+            contactList.add(contactDetails);
+            System.out.println("\nContact added successfully.\n");
+    }
+
+    /**
+     * Name : displayContacts
+     *
+     * Description : Displaying all the contact to user.
+     *
+     * Algorithm : Using for each loop to traverse through array.
+     * and printing records rows one by one.
+     *
+     * Modification : Fist Commit 29-June-2021
+     */
+    public void displayContacts()
+    {
+        for(PersonDetails element : contactList)
+        {
+            if(element != null)
+            {
+                System.out.println(element);
+            }
+        }
+    }
 }
