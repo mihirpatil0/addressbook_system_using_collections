5dbf0398c38f3f431bdde7e0dc8451a45f4b0517
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-15 23:15:47 +0530

    [mihir] Added: defined contact details in persondetails class.

--- src/com/bridgelab/addressbooksystemusingcollections/PersonDetails.java
+++ src/com/bridgelab/addressbooksystemusingcollections/PersonDetails.java
@@ -12,5 +12,90 @@ package com.bridgelab.addressbooksystemusingcollections;
 
 public class PersonDetails
 {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private long phoneNumber;
+    private String emailId;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDetails : [" +
+                "First-Name ='" + firstName + '\'' +
+                ", Last-Name ='" + lastName + '\'' +
+                ", Address ='" + address + '\'' +
+                ", City ='" + city + '\'' +
+                ", State ='" + state + '\'' +
+                ", ZipCode =" + zipCode +
+                ", Phone-Number =" + phoneNumber +
+                ", EmailId ='" + emailId + '\'' +
+                ']';
+    }
 }
